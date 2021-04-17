0d4b81385d4a5c607d0bf4f5de10317da5610dce
daitianci <502395193@qq.com>
2021-04-18 07:43:48 +0800

    oauth server refresh token

--- oauth-as/src/main/java/com/honor/springsecurity/config/SecurityConfig.java
+++ oauth-as/src/main/java/com/honor/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.honor.springsecurity.config;
 import com.honor.springsecurity.service.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,6 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(myUserDetailsService)
                 .passwordEncoder(passwordEncoder());
     }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
